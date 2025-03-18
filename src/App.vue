2e782a07673bb50c0d7b4e8a3e1629ff16feaea1
Samuel Damasceno Cardoso <82730480+Samuca1352@users.noreply.github.com>
2025-03-18 17:18:21 -0300

    att(talvez)

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
+<template>
+<div class="bg-white border-gray-200 dark:bg-gray-900 dark:bg-opacity-50 backdrop-filter backdrop-blur-lg w-screen h-screen">
+ 
+<HeaderPage ></HeaderPage>
+
+</div>
+
+</template>
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderPage from './components/HeaderPage.vue';
 </script>
 
-<template>
-  <div>
-    <div class=" text-9xl mt-5 underline decoration-1">Teste sapoha</div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style >
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
